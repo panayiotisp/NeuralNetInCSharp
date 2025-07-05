0071c39bc8ee9f82686a1e03d5c66577ed185052
Panayiotis Panayiotou <panayiotoup@dotcy.com>
2025-07-05 13:55:10 +0300

    Add project files.

---
+++ NeuralNetInCSharp/ActivationFuncs/IActivationFunction.cs
@@ -0,0 +1,21 @@
+﻿namespace NeuralNetInCSharp.ActivationFuncs {
+
+    /// <summary>
+    /// Anything that can take a real x, compute f(x), 
+    /// and also provide f′(x) for back-prop.
+    /// </summary>
+    public interface IActivationFunction {
+
+        /// <summary>
+        /// Compute f(x).
+        /// </summary>
+        double Activate(double x);
+
+        /// <summary>
+        /// Given y=f(x), compute f′(x).
+        /// </summary>
+        double Derivative(double y);
+
+    } // interface: IActivationFunction
+
+} // namespace: NeuralNetInCSharp.ActivationFuncs
